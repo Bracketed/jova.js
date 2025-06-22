6bc00b7cb152537f237a49abf2b270ce7bb1c3ea
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2025-06-23 00:25:21 +0100

    Bug fixes

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,33 +1,4 @@
-import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions';
-import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
-import { defineConfig, type Options } from 'tsup';
+import { packageConfiguration } from '@bracketed/tsup-configuration';
 
-const baseOptions: Options = {
-	clean: true,
-	splitting: true,
-	entry: ['src/**/*.ts'],
-	dts: true,
-	minify: true,
-	skipNodeModulesBundle: true,
-	sourcemap: true,
-	target: 'esnext',
-	tsconfig: 'tsconfig.json',
-	keepNames: true,
-	esbuildPlugins: [esbuildPluginVersionInjector(), esbuildPluginFilePathExtensions()],
-	treeshake: true,
-};
-
-export default [
-	defineConfig({
-		...baseOptions,
-		outDir: 'lib/cjs',
-		format: 'cjs',
-		outExtension: () => ({ js: '.cjs' }),
-	}),
-	defineConfig({
-		...baseOptions,
-		outDir: 'lib/esm',
-		format: 'esm',
-	}),
-];
+export default [...packageConfiguration];
 
